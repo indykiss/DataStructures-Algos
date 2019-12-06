28820f1775c24e1580b30e8f7c26bb443a8a8eff
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-12-06 11:46:21 -0500

    Create Algos.js

---
+++ Algos.js
@@ -0,0 +1,36 @@
+// From Facebook Itself
+// Question 1: 2D Spiral Array
+// Find the pattern and complete the function:
+// int[][] spiral(int n);
+// where n is the size of the 2D array.
+
+
+
+
+
+
+
+// Question 2: Look and Say
+// Implement a function that outputs the Look and Say sequence:
+// 1
+// 11
+// 21
+// 1211
+// 111221
+// 312211
+// 13112221
+// 1113213211
+
+// Question 3: Edit Distance
+// Write a function that returns whether two words are exactly "one edit" away using the following signature:
+// bool OneEditApart(string s1, string s2);
+// An edit is:
+// Inserting one character anywhere in the word (including at the beginning and end)
+// Removing one character
+// Replacing one character
+// Examples:
+// OneEditApart("cat", "dog") = false
+// OneEditApart("cat", "cats") = true
+// OneEditApart("cat", "cut") = true
+// OneEditApart("cat", "cast") = true
+// OneEditApart("cat", "at") = true
