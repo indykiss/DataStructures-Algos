755dd7c3793f4b2db458f233d104df4747ddb93a
indykiss <indirak@alumni.princeton.edu>
2019-12-10 04:57:15 +0700

    add stuff

--- Facebooks_Algos.js
+++ Facebooks_Algos.js
@@ -5,9 +5,32 @@
 // where n is the size of the 2D array.
 
 
-
-
-
+// I am a solution:
+var spiralOrder = function(matrix) {
+    if (matrix.length == 0 || matrix[0].length == 0)
+        return []
+    let rowBeg = 0, rowEnd = matrix.length - 1, colBeg = 0, colEnd = matrix[0].length - 1, res = [];
+    while (rowBeg <= rowEnd && colBeg <= colEnd) {
+        for (let i = colBeg; i <= colEnd; i++)
+            res.push(matrix[rowBeg][i])
+        rowBeg++;
+        for (let i = rowBeg; i <= rowEnd; i++)
+            res.push(matrix[i][colEnd])
+        colEnd--;
+        if (rowBeg <= rowEnd) {
+            for (let i = colEnd; i >= colBeg; i--)
+            res.push(matrix[rowEnd][i])
+            rowEnd--
+        }
+        if (colBeg <= colEnd) {
+            for (let i = rowEnd; i >= rowBeg; i--)
+            res.push(matrix[i][colBeg])
+            colBeg++
+        }
+        
+    }
+    return res;
+};
 
 
 // Question 2: Look and Say
--- README.md
+++ README.md
@@ -5,7 +5,6 @@ I'm practicing Facebook interview questions for fun! This repo will help me keep
 https://medium.com/@XiaohanZeng/i-interviewed-at-five-top-companies-in-silicon-valley-in-five-days-and-luckily-got-five-job-offers-25178cf74e0f
 
 
-
 ### Do me! 
 https://github.com/lolapriego/coursework
 
