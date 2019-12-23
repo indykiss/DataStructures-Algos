e2f926c5f1b7488a6d134f6e8e980847008268a6
indykiss <indirak@alumni.princeton.edu>
2019-12-23 06:17:19 -0500

    add a sorting algo

--- interviewCake.js
+++ interviewCake.js
@@ -57,9 +57,9 @@ function getMaxProfit(stockPrices) {
   getMaxProfit([10, 7, 5, 8, 11, 9]);
 
 
-  // Girl scout cookies
+// Girl scout cookies
 
-  // In order to win the prize for most cookies sold, my friend Alice and I are going to merge our Girl Scout Cookies orders and enter as one unit.
+// In order to win the prize for most cookies sold, my friend Alice and I are going to merge our Girl Scout Cookies orders and enter as one unit.
 
 // Each order is represented by an "order id" (an integer).
 
@@ -72,3 +72,70 @@ function getMaxProfit(stockPrices) {
 
 // console.log(mergeArrays(myArray, alicesArray));
 // logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+
+
+// I am a sorting algo 
+// The main algo.ie part are the edge cases 
+
+
+function mergeArraysTooSimple(myArray, alicesArray) {
+
+    // Combine the sorted arrays into one large sorted array
+    
+    // First simplest way to do this is merge then .sort()
+    // But this would be O(n log(n)) complexity. 
+    // We can do better
+    const newArr = myArray.concat(alicesArray);
+    return newArr.sort((a,b) => a - b);
+    
+    // Let's use the fact that the arrs are already sorted
+    // Edge cases: unequal arrs (main issue) and an arr is only 1 element
+    // We are going to keep track of my, alice's, and new arr's indices 
+    // so we can compare which is smallest then push into newArr
+  }
+  
+  
+  function mergeArrays(myArray, alicesArray) {
+    
+    const newArr = [];
+    
+    // Set up the variables that help us keep track of the arrs
+    let currentIndexAlices = 0;
+    let currentIndexMine = 0;
+    let currentIndexMerged = 0; 
+    
+    // We stop when the newArr has all the eles in both my & Alice's arrs 
+    while(currentIndexMerged < (myArray.length + alicesArray.length)) {
+      
+      const isMyArrDone = currentIndexMine >= myArray.length; 
+      const isAliceArrDone = currentIndexAlices >= alicesArray.length; 
+      
+      // Case: Next index in newArr is from my arr 
+      // IF my arr still is going AND EITHER  
+        // Alice's arr is done OR 
+        // At this index, my ele is less than Alice's ele at Nth index 
+        
+      if (!isMyArrDone && (isAliceArrDone || 
+      (myArray[currentIndexMine] < alicesArray[currentIndexAlices]))) {
+        
+        // We add the ele to the newArr
+        
+        newArr[currentIndexMerged] = myArrayp[currentIndexMine]; 
+        currentIndexMine++;
+        
+      // Case: Next index in newArr is from Alice
+      } else {
+        newArr[currentIndexMerged] = alicesArray[currentIndexAlice]; 
+        currentIndexAlices++
+      }
+      
+      // Either way, we increment the newArr's index so we continue adding
+      
+      currentIndexMerged++
+      
+    }
+    
+    return newArr;
+    
+  }
+
