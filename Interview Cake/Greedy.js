751087eaf5d36e1f8b7b18c0be6ebb662cbe53a2
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-01-06 14:12:43 -0500

    Add setup for new algo

--- Interview Cake/Greedy.js
+++ Interview Cake/Greedy.js
@@ -113,3 +113,18 @@ function highestProductOf3(arrayOfInts) {
   return highestProductOf3;
 }
 
+
+// You have an array of integers, and for each index you want to find the 
+// product  of every integer except the integer at that index.
+// WITHOUT using division. 
+
+// For example, given:
+//   [1, 7, 3, 4]
+
+// your function would return:
+//   [84, 12, 28, 21]
+
+// by calculating:
+//   [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+
+
