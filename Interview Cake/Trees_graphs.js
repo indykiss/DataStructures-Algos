5b7f0b902bc24b464e8275c0f43e7be1feebe459
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-01-11 17:42:57 -0500

    Update Trees_graphs.js

--- Interview Cake/Trees_graphs.js
+++ Interview Cake/Trees_graphs.js
@@ -460,3 +460,31 @@ function colorGraph(graph, colors) {
   })
 }
 
+
+  
+  
+  
+  
+/* Find the shortest path from startNode to 
+endNode, given a network.
+
+  const network = {
+  'Min'     : ['William', 'Jayden', 'Omar'],
+  'William' : ['Min', 'Noam'],
+  'Jayden'  : ['Min', 'Amelia', 'Ren', 'Noam'],
+  'Ren'     : ['Jayden', 'Omar'],
+  'Amelia'  : ['Jayden', 'Adam', 'Miguel'],
+  'Adam'    : ['Amelia', 'Miguel', 'Sofia', 'Lucas'],
+  'Miguel'  : ['Amelia', 'Adam', 'Liam', 'Nathan'],
+  'Noam'    : ['Nathan', 'Jayden', 'William'],
+  'Omar'    : ['Ren', 'Min', 'Scott'],
+  ...
+};
+
+For the network above, a message from Jayden to Adam 
+should have this route:
+
+  ['Jayden', 'Amelia', 'Adam']
+  
+*/ 
+  
