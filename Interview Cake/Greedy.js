7148b41c0a9dab38637e4f541d9ccb727b8e5304
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-01-11 21:51:23 -0500

    Update formatting

--- Interview Cake/Greedy.js
+++ Interview Cake/Greedy.js
@@ -1,21 +1,23 @@
-// Greedy algorithm: builds up a solution by choosing the option that 
-// looks the best at every step.
-// Say you're a cashier and need to give someone 67 cents (US) using as 
-// few coins as possible. 
+/*
+Greedy algorithm: builds up a solution by choosing the option that 
+looks the best at every step.
+Say you're a cashier and need to give someone 67 cents (US) using as 
+few coins as possible. 
 
 
-// How much money could I have made yesterday trading Apple stocks? 
-// Write an efficient function that takes stockPrices and returns 
-// the best profit I could have made from one purchase and 
-// one sale of one share of Apple stock yesterday.
+How much money could I have made yesterday trading Apple stocks? 
+Write an efficient function that takes stockPrices and returns 
+the best profit I could have made from one purchase and 
+one sale of one share of Apple stock yesterday.
 
+Calculate the max profit
+Greedy algo: constantly compare the prev 2 ways 
+update minPrice and maxProfit to initialize them as first price
+and first possible profit 
+*/
 
 function getMaxProfit(stockPrices) {
 
-    // Calculate the max profit
-    // Greedy algo: constantly compare the prev 2 ways 
-    // update minPrice and maxProfit to initialize them as first price
-    // and first possible profit 
     let maxProfit = stockPrices[1] - stockPrices[0];
     let minPrice = stockPrices[0];
     
@@ -41,38 +43,39 @@ function getMaxProfit(stockPrices) {
     return maxProfit; 
     // Edge case of no profit all day and input errors (no price)?
   }
-  
-  
   getMaxProfit([10, 7, 5, 8, 11, 9]);
 
 
 
-// Given an array of integers, find the highest product you can get from three of the integers.
-// Remember that numbers can be negative so [3,2,1,-100,-100] - the highest product uses 2 negatives
-// and the highest positive. So can't sort or just find the biggest 3 numbers. 
 
 
-  // Calculate the highest product of three numbers
-    // ideally in O(n) time worst case bc have to look at
-    // all the nums at least 1
+/*
+Given an array of integers, find the highest product you can get from three of the integers.
+Remember that numbers can be negative so [3,2,1,-100,-100] - the highest product uses 2 negatives
+and the highest positive. So can't sort or just find the biggest 3 numbers. 
+
+  Calculate the highest product of three numbers
+    ideally in O(n) time worst case bc have to look at
+    all the nums at least 1
   
-  // Edge cases: negative numbers. (-100)(-100)(2) = highest product
-    // given [2,1,1,-100,-100]. Less than 3 numbers given 
+  Edge cases: negative numbers. (-100)(-100)(2) = highest product
+    given [2,1,1,-100,-100]. Less than 3 numbers given 
     
+ Strategy: Loop and see if current number times
+  lowestProdOf2 or highestProdOf2 yields a new highestProdOf3
+  and current x highest/ lowest yields new highestProdOf2 / lowestProdOf2
+  and if there's a new highest / lowest value 
+*/
+
+
 function highestProductOf3(arrayOfInts) {
     
- // Strategy: Loop and see if current number times
-  // lowestProdOf2 or highestProdOf2 yields a new highestProdOf3
-  // and current x highest/ lowest yields new highestProdOf2 / lowestProdOf2
-  // and if there's a new highest / lowest value 
-    
-  // Step 1: Populate the variables to give some value to compare to within 
-    // our loop
+  // Step 1: Populate the variables to give some value to compare in loop
   let highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
   let highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
   let lowestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
-        // I don't know why we have args of eles here 
-          // instead of just arrayOfInts:
+     // I don't know why we have args of eles here 
+     // instead of just arrayOfInts:
   let highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
   let lowest = Math.min(arrayOfInts[0], arrayOfInts[1]);
         
@@ -115,32 +118,33 @@ function highestProductOf3(arrayOfInts) {
 
 
 
+/*
+You have an array of integers, and for each index you want to find the 
+product  of every integer except the integer at that index.
+WITHOUT using division. 
 
-// You have an array of integers, and for each index you want to find the 
-// product  of every integer except the integer at that index.
-// WITHOUT using division. 
+For example, given:
+  [1, 7, 3, 4]
 
-// For example, given:
-//   [1, 7, 3, 4]
+your function would return:
+  [84, 12, 28, 21]
 
-// your function would return:
-//   [84, 12, 28, 21]
+by calculating:
+  [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
 
-// by calculating:
-//   [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+Edge case: zeros and if input only has 1 integer. 
+    Negative nums are fine, just gets multipled. Same with zeros. 
+  
+  Strategy: Get the product for all numbers before the index 
+    Get the product for all numbers after the index 
+    At the index number we're looking at in our return arr, 
+      multiply together to get total product (except at index!)
+*/    
 
-// Edge case: zeros and if input only has 1 integer. 
-    // Negative nums are fine, just gets multipled. Same with zeros. 
 
 
 function getProductsOfAllIntsExceptAtIndex(intArray) {
-  
-  // Strategy: Get the product for all numbers before the index 
-    // Get the product for all numbers after the index 
-    // At the index number we're looking at in our return arr, 
-      // multiply together to get total product (except at index!)
-    
-     
+
   if(intArray.length < 2) {
     throw new Error("Can't do this");
   }
@@ -175,33 +179,33 @@ function getProductsOfAllIntsExceptAtIndex(intArray) {
   return prodOfAllExceptAtIndex;  
 }
 
-// Since only using loops, this is O(n) time
+// O(n) time: 1 loop
   // We only create 1 array, O(n) space
 
 
 
+/*
+Write a function for doing an in-place shuffle of an arr.
 
-// Write a function for doing an in-place shuffle of an arr.
+The shuffle must be "uniform," meaning each item in the original arr 
+must have the same probability of ending up in each spot in the final arr.
 
-// The shuffle must be "uniform," meaning each item in the original arr 
-// must have the same probability of ending up in each spot in the final arr.
-
-// Assume that you have a function getRandom(floor, ceiling) for getting 
-// a random integer that is >= floor and <= ceiling.
+Assume that you have a function getRandom(floor, ceiling) for getting 
+a random integer that is >= floor and <= ceiling.
 
 
-// Strategy: Randomize the order of eles in an array
-  // Make a getRandom function 
-  // As we loop, pull a random index/value from the current 
-    // index til the end of array
-  // Swap the current index/value with this new random index/value;
+Strategy: Randomize the order of eles in an array
+  Make a getRandom function 
+  As we loop, pull a random index/value from the current 
+    index til the end of array
+  Swap the current index/value with this new random index/value;
+*/
 
 function getRandom(floor, ceiling) {
   return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
 }
 
 function shuffle(array) {
-
   // Shuffle the input in place
   if(array.length < 2) {
     return array;
