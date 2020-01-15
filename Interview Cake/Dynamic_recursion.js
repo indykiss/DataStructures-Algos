c5202f9e82661ba1843de6934dbe47e9cd83af90
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-01-15 11:41:03 -0500

    Add fibonacci algo optimization

--- Interview Cake/Dynamic_recursion.js
+++ Interview Cake/Dynamic_recursion.js
@@ -52,3 +52,106 @@ function getPermutations(string) {
 
 
 
+ /*
+ 
+Write a function fib() that takes an integer nn and returns the nth Fibonacci number.
+ 
+Strat: We start with recursion, then memoize, then bottoms up. 
+
+S1: Basic recursive func. Then optimize
+
+S2: Wrap the function in a class so we can add memoizer
+
+S3: Optimize again. Bottoms up approach replaces memoizer
+
+*/ 
+
+// OK. Normal recurse. TERRIBLE run time. 
+// O(2^n) BC the call stack looks like a binary tree 
+// As we increase by 1/ 2, it's 2^1/ 2^2
+
+function fibOK(n) {
+  // Edge: 
+  if(n < 0) {
+    throw new Error("Can't have negative")
+  }
+  // Base:
+  if(n === 0 || n === 1) {
+    return n
+  }
+  // Recurse:
+  return fib(n-1) + fib(n-2)
+}
+
+
+
+// Great: I MEMOIZE. O(N) TIME AND SPACE
+class FibberGreat {
+  constructor() {
+    // Memos are hashes
+    this.memo = {}
+  }
+  
+  fib(n) {
+    // Edge case
+    if(n < 0) {
+      throw new Error("Can't do negative fib.")
+    }
+    // Base case: when to stop
+    if(n === 0 || n === 1) {
+      return n;
+    }
+    // Memoize! Have we done this already?
+      // If yes, return that guy
+    if(this.memo.hasOwnProperty(n)) {
+      return this.memo[n];
+    }
+    // Recurse. Save the recurse to move into memo hash if its not there
+    const result = this.fib(n-1) + this.fib(n-2);
+    this.memo[n] = result;
+    // Return the answer
+    return result;
+  }  
+}
+// O(n) time because since we're memoizing, we only run each call once then 
+// save. Same with space: O(n), we're saving each call once
+
+
+
+// BEST. Start with fib(1) and fib(0).
+// This is a bottom up approach! No more memo
+// O(n) time and O(1) space
+
+function fib(n) {
+    // Edge case
+    if(n < 0) {
+      throw new Error("Can't do negative fib.")
+    }
+    
+    // Base case: when to stop
+    if(n === 0 || n === 1) {
+      return n;
+    }
+    
+    // Bottoms up approach. 
+      // Build fib(0) and fib(1)
+    let prevPrev = 0 // Fib(0)
+    let prev = 1 // Fib(1)
+    let current; // Current is a thing 
+    
+    // Loop through nums and count the number of prevPrev and prev 
+      // We fib math. AND we update variables for the next iteration
+    for(let i = 1; i < n; i++) {
+      // Iterate #1: Current = 2nd fib
+      // Iterate #2: Current = 3rd fib etc... 
+      current = prev + prevPrev
+      prevPrev = prev
+      prev = current;
+    }
+    
+    // Return the answer
+    return current;
+}  
+
+
+
