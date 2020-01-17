ca471691a1ce5251cf8e6d2fcba3b09334127ffb
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-01-17 16:24:46 -0500

    Add closing parenthesis position finder algo

--- Interview Cake/Queues_stacks.js
+++ Interview Cake/Queues_stacks.js
@@ -145,3 +145,57 @@ class QueueTwoStacks {
 
 
 
+/* Find the position of the matching closing parenthesis
+in a sentence while we know the index of the opening 
+parenthesis. 
+
+Strat: 
+We loop through the sentence. As we loop, we increment 
+a counter as we see "C" in openNestedParens. 
+
+As we find a closing ")" we decrement the openNestedParens. 
+
+Once we find a closing ")" and the counter is at 0, then
+we are done and can return that position. 
+
+Lesson: For "parsing" algos, use a stack to track 
+brackets/ parentheses/ phrases/ etc that are "open"
+as we go. 
+
+This lets us store the number of items, not the items, which
+lets us use a stack with just using an integer. 
+Which is really great. 
+
+*/
+
+
+function getClosingParen(sentence, openingParenIndex) {
+  let openNestedParens = 0;
+  
+  // S1: Loop through sentence. 
+  for(let i = openingParenIndex + 1; i < sentence.length; i++) {
+    const char = sentence[i];
+    // If char is an open paren, increment
+    if(char === "(") {
+      openNestedParens++;
+    }
+    // If it's closing paren AND there's unpaired opens
+      // decrement the counter
+    if(char === ")" && openNestedParens > 0) {
+      openNestedParens--;
+    }
+    // If it's a closing paren AND no unpaired opens 
+      // Return position
+    if(char === ")" && openNestedParens === 0) {
+      return i;
+    }
+  }
+  throw new Error("No closing parenthesis")
+}
+
+// O(n) time because looping, which is worst case and also 
+  // the minimum because we have to look at everything 
+  // at least once
+// O(1) space because we only have a constant to store
+
+
