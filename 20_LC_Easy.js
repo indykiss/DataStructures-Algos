079c5726cfd3b421c2daa843ed8762c00903632d
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-01-22 12:43:32 -0500

    Add valid anagram algo

--- 20_LC_Easy.js
+++ 20_LC_Easy.js
@@ -5,6 +5,7 @@
 14. Reverse integer
 15. Fizz Buzz
 16. Reverse String
+17. Valid anagram
 
 
 */
@@ -247,3 +248,46 @@ var reverseString = function(s) {
         j--;
     }    
 }
+
+
+
+
+
+
+
+/*
+Valid Anagram
+
+Given two strings s and t , write a function 
+to determine if t is an anagram of s.
+
+Define an anagram: two strings with same number of letters
+
+Strat: Create a map for both strings. Sort to be alphabetical
+If maps are equal, it's an anagram . 
+
+Assumptions: All lower case
+*/
+
+
+var isAnagram = function(s, t) {
+    // Keep 1 map to count common characters & number of
+        // times they appear 
+    const count = {};
+    // Count the chars in S
+    for(let i = 0; i < s.length; i++) {
+        let char = s[i]
+        count[char] = (count[char] || 0) + 1;
+    }
+    // Decrement char count in t
+    for(let i = 0; i < t.length; i++) {
+        let char = t[i]
+        if(!count[char]) {return false;}         
+        count[char]--;
+    }   
+    // If not length equal, false else true
+    if(s.length !== t.length) {
+        return false;
+    }
+    return true;
+};
