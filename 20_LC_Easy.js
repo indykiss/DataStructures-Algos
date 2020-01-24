c9ad674b0604d987a1c6c82f6ad75e9f6fa1007d
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-01-24 17:58:09 -0500

    Add design hashmap algo

--- 20_LC_Easy.js
+++ 20_LC_Easy.js
@@ -8,6 +8,7 @@
 17. Valid anagram
 18. Subtree of another tree
 19. Roman to integer
+20. Design HashMap
 
 
 */
@@ -431,3 +432,41 @@ var romanToInt = function(s) {
 };
 
 
+
+
+
+/*
+Design HashMap 
+Design a HashMap without using any built-in hash table libraries.
+
+To be specific, your design should include these functions:
+
+put(key, value) : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
+
+get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+
+remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
+*/
+
+
+var MyHashMap = function() {
+    this.map = [];
+};
+
+// Value will always be non-negative. 
+MyHashMap.prototype.put = function(key, value) {
+    this.map[key] = [key, value]
+};
+
+/*Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+MyHashMap.prototype.get = function(key) {
+    if(this.map[key] === undefined) {
+        return -1
+    }
+    return this.map[key][1];
+};
+
+/*Removes the mapping of the specified value key if this map contains a mapping for the key */
+MyHashMap.prototype.remove = function(key) {
+    this.map[key] = undefined;
+};
