77d2e7bd05aef4ac5203d289e4b7abf072b03a7c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-01-24 18:51:53 -0500

    Add spiral matrix algo

--- 10_LC_medium.js
+++ 10_LC_medium.js
@@ -9,6 +9,7 @@ Index of 10 medium leetcodes
 6. Number of islands 
 7. Rotten oranges 
 8. Longest palindrome substring
+9. Spiral Matrix -- not done. Do again
 
 
 */
@@ -538,4 +539,58 @@ var longestPalindrome = function(s) {
         }
     }
     return max;
+};
+
+
+
+
+
+
+/*
+Spiral Matrix
+Given a matrix of m x n elements (m rows, n columns), 
+return all elements of the matrix in spiral order.
+
+Treat each row differently.
+Loop as many times there are columns. 
+
+Lesson: 
+.concat() merges arrays 
+.push() adds elements to the end of an array
+
+Close!! ONE OFF
+
+*/
+
+var spiralOrder = function(matrix) {
+    // Edge: Bad inputs 
+    if(!matrix) {return false}
+    if(!matrix[0]) {return false}
+    
+    let arr = [];
+    
+    // Top row
+    arr = arr.concat(matrix.shift());
+    
+    // Right edge 
+    for(let i = 0; i < matrix.length - 1; i++) {
+        arr.push(matrix[i].pop());
+    }
+    
+    // Last row, in reverse order 
+    const lastRow = matrix.pop()
+    
+    if(lastRow) {
+        arr = arr.concat(lastRow.reverse());
+    }
+    
+    // Add rest of the eles
+    for(let i = matrix.length - 1; i>=0; i--) {
+        if(matrix[i].length) {
+            arr.push(matrix[i].shift());
+        }
+    }
+    
+    
+    return arr.concat(spiralOrder(matrix));
 };
