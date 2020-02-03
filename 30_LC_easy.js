ca8cef1e0d32c634eecb3042ca7b5b25e6e67d2c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-02-03 17:31:47 -0500

    Add most common word algo

--- 30_LC_easy.js
+++ 30_LC_easy.js
@@ -1,6 +1,6 @@
 /* Index:
 31. Move Zeroes
-
+32. Most Common Word
 
 */
 
@@ -32,4 +32,43 @@ var moveZeroes = function(nums) {
             nums.push([0]);
         }
     }
+};
+
+
+
+
+/*
+Most Common Word
+
+Given a paragraph and a list of banned words, return the most frequent word that is not in the list of banned words.  It is guaranteed there is at least one word that isn't banned, and that the answer is unique.
+
+*/
+
+
+
+var mostCommonWord = function(paragraph, banned) {
+    // Make everything lower case
+        // Split it based on space and punctuation
+    let words = paragraph.toLowerCase()
+    .replace(/[!?',;\.]/g, ' ')
+    .split(/\s+/g);
+    
+    const wordCount = [];
+    
+    for(let i = 0; i < words.length; i++) {
+        let word = words[i]
+
+        if(!banned.includes(word)) {                
+            if(wordCount[word]) {
+                wordCount[word] = wordCount[word] + 1;
+            } else {
+                wordCount[word] = 1;
+            }
+        }
+    }
+    
+    let sortedWordCount = Object.keys(wordCount)
+    .sort((a,b) => wordCount[b] - wordCount[a])
+    
+    return sortedWordCount[0];
 };
