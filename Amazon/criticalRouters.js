d11bced7deca0a77ddee02ed3d0235397cd219a8
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-02-15 15:25:22 -0500

    Add critical connections algo

---
+++ Amazon/criticalRouters.js
@@ -0,0 +1,107 @@
+/*
+
+Critical connections in a network
+... a network from hell 
+
+There are n servers numbered from 0 to n-1 connected by undirected server-to-server 
+connections forming a network where connections[i] = [a, b] represents a connection between 
+servers a and b. Any server can reach any other server directly or indirectly through the network.
+
+A critical connection is a connection that, if removed, will make some server unable to reach some other server.
+
+Return all critical connections in the network in any order.
+
+
+SAME WAY TO PHRASE THE ABOVE:
+
+You are given an undirected connected graph. An articulation point (or cut vertex) is 
+defined as a vertex which, when removed along with associated edges, makes the graph disconnected 
+(or more precisely, increases the number of connected components in the graph). The task is to find all 
+articulation points in the given graph.
+
+Input: The input to the function/method consists of three arguments:
+
+numNodes, an integer representing the number of nodes in the graph.
+numEdges, an integer representing the number of edges in the graph.
+edges, the list of pair of integers - A, B representing an edge between the nodes A and B.
+
+Output: Return a list of integers representing the critical nodes.
+
+Example:
+
+Input: numNodes = 7, numEdges = 7, edges = [[0, 1], [0, 2], [1, 3], [2, 3], [2, 5], [5, 6], [3, 4]]
+
+[Write out the graph]
+
+Ouput: [2, 3, 5]
+
+
+Tip: For two particularly hard tests to pass, you r result should contain edges such that for an edge (a, b) 
+a < b. i.e. if you have a critical edge (2,1), change it to (1, 2) when adding to resultant list.
+*/
+
+
+/*
+Strategy:
+Find the edges that do not lead to a cycle. 
+
+Use a DFS to look at the nodes. 
+
+rank = time stamp of the DFS
+minObs = lowest rank seen on the path we're on (as we're traversing)
+
+Ya I have no idea what's going on here. 
+
+*/
+
+const criticalConnections = (n, connections) => {
+    // Create the graph to traverse through
+    const graph = createGraph(n, connections);
+
+    // Don't traverse over things we've already seen 
+    const visited = new Set();
+    let rank = 0;
+    const output = [];
+    
+    // Make a DFS function
+    function dfs(node, parent) {
+        visited.add(node.val);
+        // Lowest rank and rank are default rank
+        node.rank = rank;
+        node.minObs = rank;
+        // Increment rank as we go
+        rank++;
+        // DFS. Don't visit parents. Just visit children nodes 
+        node.children.forEach((child) => {
+            if(child === parent) {return;}
+            // If we've seen the child already, just peek at its value
+            if(visited.has(child)) {
+                node.minObs = Math.min(node.minObs, graph[child].minObs);
+                return;
+            } else {                                                                                                      
+                // Traverse and update the minObs to take the min value if its min
+                node.minObs = Math.min(node.minObs, dfs(graph[child], node.val));
+            }
+            // If node.rank > child.minObs, the edge between node and child
+            // is a bridge since that edge will never lead back to the node itself along the way.
+            if (node.rank < graph[child].minObs) output.push([node.val, child]);
+        })
+        return node.minObs;
+    };
+    
+    dfs(graph[connections[0][0]], connections[0][0]);
+    return output;
+};
+
+
+function createGraph(n, connections) {
+    const output = {};
+    for (let i = 0; i < n; i++) {
+        output[i] = { val: i, children: [], rank: -Infinity, minObs: Infinity };
+    }
+    connections.forEach((connection) => {
+        output[connection[0]].children.push(connection[1]);
+        output[connection[1]].children.push(connection[0]);
+    });
+    return output;
+}
