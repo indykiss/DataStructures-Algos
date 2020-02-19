f3f175c61db391d013da55cb589ab67920c72406
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-02-19 15:16:37 -0500

    Update Hackerranks_algos.js

--- Hackerranks_algos.js
+++ Hackerranks_algos.js
@@ -152,3 +152,32 @@ function repeatedString(s, n) {
     newStr.concat(s.slice[0, remainder + 1]);
     return newStr;
 }
+
+
+/*
+Count the number of times a appears in an string with 
+an infinite length
+
+- Repeat the string of s until the output's length = n
+- In this new string, count how many times a appears
+
+Strategy:
+- How often does the string get repeated
+- Compute this number then multiply that by 
+the number of "a"s that are inthe string
+*/
+
+// Sample input: (aabc, 10) -> aabcaabcaa -> Output: 6
+// Complete the repeatedString function below.
+function repeatedString(s, n) {
+    const repeats = n % s.length;
+    const repeatMath = (n - repeats)/ s.length;
+    let count = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === "a") {
+            count += repeatMath + (i < repeats);
+        }
+    }
+    return count;
+}
