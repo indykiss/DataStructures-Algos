dbb3a1682a8463e18362ac3003dfc7a44f2066a1
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-03-11 17:23:12 -0400

    Add the rest of the hackerranks I need to do

---
+++ HackerRank/ToDo/Hacker_ranks.js
@@ -0,0 +1,64 @@
+/*
+
+This is a list of the hackerranks that I need to work on for the next week. 
+Divided by section. Top of each section are the easier Qs. 
+
+Recursion: 
+- Recursive digital sum: https://www.hackerrank.com/challenges/recursive-digit-sum/problem 
+- Crossword puz: https://www.hackerrank.com/challenges/crossword-puzzle/problem 
+- Davis staircase: https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem 
+
+Hash tables:
+- Frequency queries: https://www.hackerrank.com/challenges/frequency-queries/problem 
+- Ice cream shop: https://www.hackerrank.com/challenges/icecream-parlor/problem 
+  - Buy all the ice cream in the whole universe basically 
+- Count triplets: https://www.hackerrank.com/challenges/count-triplets-1/problem 
+- Sherlock and anagrams: https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem  
+
+Arrays:
+- Minimum swaps to sort array 
+- Pairs: https://www.hackerrank.com/challenges/pairs/problem  
+- Array manipulation: https://www.hackerrank.com/challenges/crush/problem 
+
+Trees:
+- Swap nodes in binary tree: https://www.hackerrank.com/challenges/swap-nodes-algo/problem 
+
+Sorting:
+- Fradulent activity notifications: https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem 
+- Counting inversions: https://www.hackerrank.com/challenges/ctci-merge-sort/problem 
+
+Queues:
+- Castle on the grid: https://www.hackerrank.com/challenges/castle-on-the-grid/problem 
+
+Stacks:
+- Balanced brackets: https://www.hackerrank.com/challenges/balanced-brackets/problem 
+- Largest rectangle: https://www.hackerrank.com/challenges/largest-rectangle/problem 
+- Min Max riddle: https://www.hackerrank.com/challenges/min-max-riddle/problem 
+- Poisonous plants
+https://www.hackerrank.com/challenges/poisonous-plants/problem 
+
+
+Greedy algos:
+- Max min array: https://www.hackerrank.com/challenges/angry-children/problem 
+- Greedy florist: https://www.hackerrank.com/challenges/greedy-florist/problem 
+
+
+Graphs: 
+- Find the nearest clone: https://www.hackerrank.com/challenges/find-the-nearest-clone/problem 
+- DFS: Rotten orange / Connected Cells in a Grid 
+- Choosing in a graph:
+  A kingdom has cities connected by bidirectional roads. There's a unique path between any pair of cities. 
+  Machines are rising up the destroy the whole kingdom. 
+  If two machines work together, they'll attack. 
+  We need to destroy the roads that connect thecities with machines. 
+- Balanced forest
+https://www.hackerrank.com/challenges/balanced-forest/problem 
+
+Search: 
+- Minimum time required: https://www.hackerrank.com/challenges/minimum-time-required/problem 
+- Making candies:
+https://www.hackerrank.com/challenges/making-candies/problem 
+
+
+
+*/
