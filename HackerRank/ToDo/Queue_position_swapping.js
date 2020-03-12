0730ec736389f8b8ff14dfef2e5d2e1ffb60020d
Indira Kissoondyal <indirak@alumni.princeton.edu>
2020-03-12 08:54:12 -0400

    Add structure for swapping q

---
+++ HackerRank/ToDo/Queue_position_swapping.js
@@ -0,0 +1,18 @@
+/*
+Queue Position swapping 
+
+People are waiting in line. Each have a sticker with their original position. 
+People can swap places with the person directly in front of them, up to two times but no more. 
+Find the minimum number of swaps needed to get back to the original order of 1 -> n. 
+
+So the correct order: [n = i + 1 for ALL values]. Find the difference between what the actual value is and what we're seeing BUT only 1 way, we don't want to double count. Increment count. 
+
+If diff is > 2, then "too chaotic."
+*/
+
+
+// Complete the minimumSwaps function below.
+
+function minimumSwaps(q) {
+   
+}
