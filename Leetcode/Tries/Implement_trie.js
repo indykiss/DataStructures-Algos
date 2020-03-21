fdc1d1ce3e851e82055af99fe1762d53ecd38968
indykiss <indirak@alumni.princeton.edu>
2020-03-21 18:21:29 -0400

    add top kth frequent words

---
+++ Leetcode/Tries/Implement_trie.js
@@ -0,0 +1,60 @@
+
+/*
+Implement a trie
+
+Implement a trie with insert, search, and startsWith methods.
+
+Ex: 
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");   
+trie.search("app");     // returns true
+
+
+Strategy:
+- YA IDK I JUST COPIED AN ANSWER. 
+- Really really need to do this again
+*/
+
+
+class Trie {
+    constructor() {
+     this.root = {};
+    }
+    
+    insert(word) {
+        let node = this.root; 
+        for(let char of word) {
+            if(node[char] == null) {
+                node[char] = {} 
+            }
+            node = node[char]                    
+        }
+        node.isWord = true;   
+    }
+    
+    traverse(word) {
+        let node = this.root;
+        for(let char of word) {
+            node = node[char]
+            if(node == null) {
+                return null; 
+            } 
+        }
+        return node;
+    }
+    
+    search(word) {
+        const node = this.traverse(word);
+        return node != null && node.isWord === true;
+    }
+    
+    startsWith(prefix) {
+        return this.traverse(prefix) != null;
+    }
+    
+}
+
