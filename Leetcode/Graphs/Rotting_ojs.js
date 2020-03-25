67cbaf5a14834e04fbdad81c1f48bfc93d17fb25
indykiss <indirak@alumni.princeton.edu>
2020-03-25 19:32:58 -0400

    add rotten oranges

--- Leetcode/Graphs/Rotting_ojs.js
+++ Leetcode/Graphs/Rotting_ojs.js
@@ -15,6 +15,99 @@ Keep a counter for every time BFS was called. Increment. Return count at the end
 */
 
 
+
+
+// Attempt #2: Amazon loves me. Do me again 
+
+var orangesRotting = function(grid) {
+    
+    if(!grid) {return false};
+    
+    let count = 0;
+    let rotten = [];
+    
+    // Populates the queue with the rotten oranges' positions
+    for(let row = 0; row < grid.length; row++) {
+        for(let col = 0; col < grid[row].length; col++) {
+            if(grid[row][col] === 2) {
+                rotten.push([row, col])
+            }
+        }
+    }
+    // Walk through the grid calling BFS on every item 
+    while(grid) {
+        rotten = bfs(grid, rotten); 
+        if(rotten.length === 0) {
+            break;
+        }
+        count += 1;
+    }
+    // Checks if there's an orange that's impossible to make rotten. AFTER we are done with our breadth first search
+    if(isImpossible(grid)) return -1 
+    
+    return count;
+};
+
+
+const bfs = function(grid, rotten) {
+    const queue = [];
+    const row = grid.length - 1;
+    const col = grid[0].length - 1;
+    
+    while(rotten.length > 0) {
+        const first = rotten.shift();
+        const x = first[0];
+        const y = first[1];
+        
+        // Look left
+        if(x > 0 && grid[x-1][y] == 1) {
+            queue.push([x-1, y]);
+            grid[x-1][y] = 2;
+        }        
+        // Look right
+        if(x < row && grid[x+1][y] == 1) {
+            queue.push([x+1, y]);
+            grid[x+1][y] = 2; 
+        }
+        // Look up
+        if(y > 0 && grid[x][y-1] == 1) {
+            queue.push([x, y-1]);
+            grid[x][y-1] = 2; 
+        }
+        // Look down 
+        if(y < col && grid[x][y+1] == 1) {
+            queue.push([x, y+1]);
+            grid[x][y+1] = 2; 
+        }
+    }
+    return queue;
+}
+
+
+const isImpossible = function(grid) {
+    // IF at the end of the bfs, if we have any oranges left
+    for(let row = 0; row < grid.length; row++) {
+        for(let col = 0; col < grid[row].length; col++) {
+            if (grid[row][col] === 1) {
+                return true; 
+            }
+        }
+    }
+    // IF there's a fresh orange that we can't make bad 
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+// Attempt #1
+
 var orangesRotting = function(grid) {
     
     // Edge: Valid input
