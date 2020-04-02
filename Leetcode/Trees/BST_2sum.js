46458ab73bff0ed349b9b0fc1a4fdd65dcd895f0
indykiss <indirak@alumni.princeton.edu>
2020-04-02 13:03:57 -0400

    add bst two sum

---
+++ Leetcode/Trees/BST_2sum.js
@@ -0,0 +1,33 @@
+/*
+Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+Example 1:
+
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 9
+
+Output: True
+*/
+
+
+var findTarget = function(root, k, seen = []) {
+
+    // Base case
+    if(root === null) return false; 
+    
+    let left = root.left;
+    let right = root.right; 
+    
+    if(seen.includes(k - root.val)) {
+        return true; 
+    }
+    seen.push(root.val);
+    
+    return findTarget(left, k, seen) || findTarget(right, k, seen)   
+};
