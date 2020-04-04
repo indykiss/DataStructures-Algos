dc01b01200ed566c1523faf39718a99a6b789ac4
indykiss <indirak@alumni.princeton.edu>
2020-04-04 13:43:06 -0400

    add bst two sum

--- Leetcode/Trees/BST_2sum.js
+++ Leetcode/Trees/BST_2sum.js
@@ -18,6 +18,28 @@ Output: True
 */
 
 
+
+// Attempt 2
+// Did ok but off
+var findTarget = function(root, k, seen = []) {        
+    if(root === null) return false;     
+        
+    let left = root.left; 
+    let right = root.right; 
+    let partner = k - root.val;
+    
+    if(seen.includes(partner)) {
+        return true;
+    }
+    
+    seen.push(root.val);
+    
+    return findTarget(left, k, seen) || findTarget(right, k, seen)
+};
+
+
+
+// Attempt 1
 var findTarget = function(root, k, seen = []) {
 
     // Base case
