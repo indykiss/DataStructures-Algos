2ec89fb1339e57a6de64c5ed2e76215701cc8485
indykiss <indirak@alumni.princeton.edu>
2020-04-09 12:11:43 -0400

    add sliding window pattern

--- Leetcode/Sliding window/intro.js
+++ Leetcode/Sliding window/intro.js
@@ -49,3 +49,41 @@ const max_sub_array_of_size_k = function(k, arr) {
     }
     return maxSum;
   }
+
+
+
+/*   
+Given an array of positive numbers and a
+positive number ‘S’, find the length of the
+smallest contiguous subarray whose sum is 
+greater than or equal to ‘S’. Return 0, if
+no such subarray exists.
+
+Input: [2, 1, 5, 2, 3, 2], S=7 
+Output: 2
+Explanation: The smallest subarray with a sum great 
+than or equal to '7' is [5, 2].
+
+*/
+
+
+
+function smallest_subarray_with_given_sum(s, arr) {
+    let windowSum = 0,
+      minLength = Infinity,
+      windowStart = 0;
+  
+    for (windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+      windowSum += arr[windowEnd]; // add the next element
+      // shrink the window as small as possible until the 'window_sum' is smaller than 's'
+      while (windowSum >= s) {
+        minLength = Math.min(minLength, windowEnd - windowStart + 1);
+        windowSum -= arr[windowStart];
+        windowStart += 1;
+      }
+    }
+  
+    if (minLength === Infinity) return 0;
+
+    return minLength;
+}
