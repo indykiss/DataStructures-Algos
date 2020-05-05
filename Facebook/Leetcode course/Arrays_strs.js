216d4c05a0656db08a8e8a626f055cad6df0187f
indykiss <indirak@alumni.princeton.edu>
2020-05-04 23:28:10 -0400

    add atoi

--- Facebook/Leetcode course/Arrays_strs.js
+++ Facebook/Leetcode course/Arrays_strs.js
@@ -32,3 +32,40 @@ var lengthOfLongestSubstring = function(s) {
     return maxLength;
 } 
 
+
+
+/*
+Atoi
+
+Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+
+*/
+
+var myAtoi = function(str) {
+
+    const min = Math.pow(-2, 31);
+    const max = min * - 1 - 1
+
+    let s = str.trim();
+    if(s === '') return 0;
+
+    s = s.split(' ');
+    s = parseInt(str[0]);
+
+    if(Number.isNaN(s)) {
+        return 0;
+    } else if (s < min) {
+        return max;
+    } else {
+        return s;
+    }
+    
+}
