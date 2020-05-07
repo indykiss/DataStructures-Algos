481f8d787ca59930cc7bd46234aea20f5c4e8083
indykiss <indirak@alumni.princeton.edu>
2020-05-06 22:24:33 -0400

    begin ctci

---
+++ CTCI/Ch1_Arrs_Strs.js
@@ -0,0 +1,17 @@
+/* May 06, 2020
+My plan for the next 4-5 weeks is to study Cracking
+the coding interview. 
+
+It's a classic and one I haven't done tons with. 
+I read the chapters but didn't do all the problems, 
+so I'm starting from scratch at chapter 1. 
+
+I'm using this repo to track my progress. 
+
+Ideally, I can finish ~4 chapters a week. Then have 1
+week totally off. If I can't finish ~4 chapters, then 
+I have an extra week to work on it. Plus still take a 
+couple of days off. 
+I'm building in an automatic Sunday off rest day. :) 
+Hopefully that'll prevent burnout. 
+*/
