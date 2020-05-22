8d7f659e2979c0af02d5414483ba76d77cdff329
indykiss <indirak@alumni.princeton.edu>
2020-05-22 15:51:07 -0400

    start ch3 trees and graphs

---
+++ CTCI/Ch4_Trees_Graphs.js
@@ -0,0 +1,26 @@
+
+/*
+Trees and graphs aren't linear, so harder to traverse. 
+Worst and average time can vary really wildly too. 
+
+Binary Tree = tree with every branch having 2 or fewer leaves/ children brances 
+Binary Search Tree = Binary tree where the left node is smaller than the parent 
+node and the right child is larger than the parent node. 
+
+Balanced vs unbalanced trees:
+
+A BST is balanced when the left and right subtrees' heights differ by 1 level at the most. AND the left subtree is balanced AND the right subtree is balanced. 
+    - Note: A tree can be height or weight balanced. One does not affect the other. 
+    - https://stackoverflow.com/questions/8015630/definition-of-a-balanced-tree 
+
+A full and complete tree is when all the leaves in a tree are at the bottom of
+the tree and all non-leaf nodes have exactly two children. 
+
+Traversing a tree:
+
+In-order:
+Post-order: 
+Pre-order: 
+
+*/
+
