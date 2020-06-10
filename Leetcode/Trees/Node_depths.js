a0e18e871dde293b48e72c0ef9996df4e77609c3
indykiss <indirak@alumni.princeton.edu>
2020-06-09 21:02:34 -0400

    add depth of nodes

--- Leetcode/Trees/Node_depths.js
+++ Leetcode/Trees/Node_depths.js
@@ -24,3 +24,11 @@ themselves or none/null.
 
 */
 
+
+
+function nodeDepths(root, depth = 0) {
+	if(!root) return 0; 
+		
+	return depth + nodeDepths(root.left, depth + 1) +
+	nodeDepths(root.right, depth + 1);	
+}
