7841e60b1335916360dd47da5351a29a448ac846
indykiss <indirak@alumni.princeton.edu>
2020-06-09 20:53:25 -0400

    add set up for node depths

---
+++ Leetcode/Trees/Node_depths.js
@@ -0,0 +1,26 @@
+
+
+/*
+Node depths 
+
+The distance between a node in a BT and the tree's root is called
+its depth.
+
+Write a function that takes in a binary tree and returns the 
+sum of its nodes depths. 
+
+Each binary tree node has an integer value, a left child node, and 
+a right child node. Children nodes can either be binary tree nodes
+themselves or none/null. 
+
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+
+ Depths: 6 
+
+*/
+
