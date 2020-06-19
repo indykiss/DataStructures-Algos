94eba7425e5c0294a0938cbc6d0c1c93efc5c3e9
indykiss <indirak@alumni.princeton.edu>
2020-06-18 22:10:39 -0400

    add notes to bubble sort

--- Leetcode/Sorts/Bubblesort.js
+++ Leetcode/Sorts/Bubblesort.js
@@ -11,6 +11,17 @@ until it's all sorted.
 */
 
 
+// O(n^2) time and O(1) space
+	// Need to do a second pass through
+	// to check that we're done 
+	// If we go through the array in a round
+    // w/o swapping? Yes. Ok done
+    
+    // We use the counter to count how many numbers in the arr
+    // have been sorted at the end. Ex: we know after 1 round
+    // of [4,3,2,1] that [3,2,1,4] the 4 is done. So 
+    // we make the loop through this array smaller as we go. 
+    // Slightly optimizes the algo
 function bubbleSort(arr) {
 	let isSorted = false;
 	let counter = 0;
