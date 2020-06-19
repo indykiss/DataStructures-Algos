df2ea49db930d93e7abcec53127b16ecab5419fb
indykiss <indirak@alumni.princeton.edu>
2020-06-18 21:20:42 -0400

    add find 3 largest nums in an arr

--- Leetcode/Warm up/Find_3_largest_nums.js
+++ Leetcode/Warm up/Find_3_largest_nums.js
@@ -1,6 +1,41 @@
 
 
 
+/*
+Find the three largest numbers in an array.
+*/
+
+// O(n) time and O(1) space
+function findThreeLargestNumbers(array) {
+	const three = [null, null, null];
+	for(const num of array) {
+		updateLargest(three, num)
+	}
+	return three;
+}
+
+function updateLargest(three, num) {
+	if(three[2] === null || num > three[2]) {
+		shiftAndUpdate(three, num, 2);
+	} else if(three[1] === null || num > three[1]) {
+		shiftAndUpdate(three, num, 1);
+	} else if(three[0] === null || num > three[0]) {
+		shiftAndUpdate(three, num, 0);
+	}
+}
+
+function shiftAndUpdate(array, num, index) {
+	for(let i = 0; i <= index; i++) {
+		if(i === index) {
+			array[i] = num;
+		} else {
+			array[i] = array[i + 1];
+		}
+	}
+}
+
+
+
 
 // close but not there yet
 function findThreeLargestNumbers(array) {
