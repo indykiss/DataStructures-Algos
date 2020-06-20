6ced1d67fb8f7405f65f0f6f2d860fbc60004a4f
indykiss <indirak@alumni.princeton.edu>
2020-06-20 00:06:57 -0400

    add insertion sort

---
+++ Leetcode/Sorts/Insertion_sort.js
@@ -0,0 +1,25 @@
+
+/*
+Insertion sort
+
+.sort() is an insertion sort. 
+
+O(n^2) time and O(1) space
+*/
+
+function insertionSort(array) {
+    for(let i = 1; i < array.length; i++) {
+          let j = i; 
+          while(j > 0 && array[j] < array[j - 1]) {
+              swap(i, j-1, array)
+              j -= 1;
+          }
+      }
+      return array;
+  }
+  
+  function swap(i, j, array) {
+      const temp = array[j];
+      array[j] = array[i];
+      array[i] = temp;
+  }
