f0d48650af9c56efea100d0def7227716946286a
indykiss <indirak@alumni.princeton.edu>
2020-06-30 23:06:07 -0400

    add spiral traverse

--- Leetcode/All/2D_spiral_arr.js
+++ Leetcode/All/2D_spiral_arr.js
@@ -15,7 +15,45 @@ Input:
   Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 Note: From facebook, not leetcode 
+Algoexpert also
 */
+
+function spiralTraverse(array) {
+	const res = [];
+  let startRow = 0,
+		endRow = array.length - 1;
+	let startCol = 0, 
+		endCol = array[0].length - 1;
+	
+	while(startRow <= endRow && startCol <= endCol) {
+		for(let col = startCol; col <= endCol; col++) {
+			res.push(array[startRow][col]);
+		}
+		for(let row = startRow + 1; row <= endRow; row++) {
+			res.push(array[row][endCol]);
+		}
+		// When center of matrix is 1 ROW, not a square
+		// We don't want to double count this row
+		for(let col = endCol - 1; col >= startCol; col--) {
+			if(startRow == endRow) break;
+			res.push(array[endRow][col]);
+		}
+		// When center of matrix is 1 COLUMN, not a square
+		// We don't want to double count this col 
+		for(let row = endRow - 1; row > startRow; row--) {
+			if(startCol == endCol) break;
+			res.push(array[row][startCol]);
+		}
+		
+		startRow++;
+		endRow--;
+		startCol++;
+		endCol--;
+	}
+	
+	return res;
+}
+
 
 
 var spiralOrder = function(matrix) {
