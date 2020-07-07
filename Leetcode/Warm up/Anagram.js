b9c76b802e3d57559e288fbba0b59716d9c2b06b
indykiss <indirak@alumni.princeton.edu>
2020-07-06 18:13:09 -0800

    practice warmup

--- Leetcode/Warm up/Anagram.js
+++ Leetcode/Warm up/Anagram.js
@@ -12,6 +12,36 @@ If maps are equal, it's an anagram .
 Assumptions: All lower case
 */
 
+// aabbcc, ccbbaa => true 
+// abx, xbc => falce
+var isAnagram = function(s,t) {
+    // S1: Either alphabetize all the chars in the str and 
+        // compare them to each other 
+    // OR count the letters in a hash
+    let countS = {};
+    let countT = {};
+
+    for(let char of s) {
+        if(countS.has(char)) {
+            countS[char]++
+        } else {
+            countS[char] = 1;
+        }
+    }
+    for(let char of t) {
+        if(countT.has(char)) {
+            countT[char]++
+        } else {
+            countT[char] = 1;
+        }
+    }
+    // compare the two hashes
+        // problem with hash order?
+    if(countT == countS) return true; 
+    return false; 
+
+}
+
 
 var isAnagram = function(s, t) {
     // Keep 1 map to count common characters & number of
