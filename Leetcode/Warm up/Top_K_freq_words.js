ed944397809d5e017ef1c69f0c93f5ac0b83cb3d
indykiss <indirak@alumni.princeton.edu>
2020-08-14 19:36:36 -0800

    add top k freq words

--- Leetcode/Warm up/Top_K_freq_words.js
+++ Leetcode/Warm up/Top_K_freq_words.js
@@ -86,3 +86,26 @@ var topKFrequent = function(words, k) {
 };
 
 
+
+var topKFrequent = function(words, k) {
+    // Make a hash of word: occurences
+    let hash = {};
+    for(let i = 0; i < words.length; i++) {
+        if(hash[words[i]]) {
+            hash[words[i]] = hash[words[i]] + 1;
+        } else {
+            hash[words[i]] = 1;
+        }
+    }
+    // Sort the keys by char number of occurences in ties, make it alphabetical 
+    let sortedHash = Object.keys(hash).sort((a,b) => {
+        let temp = hash[b] - hash[a]
+        if(hash[b] === hash[a]) {
+            return a.localeCompare(b)
+        } else {
+            return temp;
+        }
+    })
+    // Slice from 0 to K
+    return sortedHash.slice(0,k);
+} 
