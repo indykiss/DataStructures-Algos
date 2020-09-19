f524251cf2d96c2c5830d495479a96e01710707d
indykiss <indirak@alumni.princeton.edu>
2020-09-19 17:06:04 -0400

    add house robber algo

--- Leetcode/Fall Practice/LC_Easy_TopQs.js
+++ Leetcode/Fall Practice/LC_Easy_TopQs.js
@@ -201,6 +201,38 @@ var maxSubArray = function(arr) {
 
 
 /*
+House Robber
 
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, the only constraint 
+stopping you from robbing each of them is that adjacent houses have 
+security system connected and it will automatically contact the police 
+if two adjacent houses were broken into on the same night.
 
+Given a list of non-negative integers representing the amount of money of 
+each house, determine the maximum amount of money you can rob tonight without 
+alerting the police.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+    Total amount you can rob = 1 + 3 = 4.
 */
+
+var robDaCribs = function(arr) {
+    let prevMax = 0,
+        currMax = 0;
+
+    for(let num of arr) {
+        let temp = currMax; 
+        currMax = Math.max(prevMax + num, currMax);
+            // f(k) = max(f(k – 2) + Ak, f(k – 1)) 
+            // curr max is going to be either the curr max or 
+            // f(k) = Largest amount that you can rob from the first k houses.
+            // Ai = Amount of money at the ith house.
+        prevMax = temp;
+    }
+    return currMax;
+}
