e1b71af9a2fbbf35e8a9f7c4130bb740042b7d81
indykiss <indirak@alumni.princeton.edu>
2020-09-20 16:05:42 -0400

    add interwing.io practice q

--- Leetcode/Fall Practice/Interviewingio.js
+++ Leetcode/Fall Practice/Interviewingio.js
@@ -45,3 +45,74 @@ function pickFromSet(set, offLimits) {
  }
  
  santaSorter(["A", "B", "C"]);
+
+
+
+
+
+
+/*
+Goal: Write a function that takes in 2 dates and returns T/F if they are 1 month apart. 
+
+Ex:
+{Day: 10, Month: 09, Year: 2020}, {Day: 10, Month: 10, Year: 2019} => False
+
+{Day: 10, Month: 09, Year: 2020}, {Day: 10, Month: 10, Year: 2020} => True
+
+Notes:
+- Ask more clarifying questions upfront, before I start coding
+- Think about ambiguities first before coding 
+- I should do a quick project before continuing leetcodes. I shouldn't have forgotten
+how to access elements in a hash and confusion about return/ console.log
+*/
+
+
+
+// If there's a new year
+// checkerIfMonthApart({Day: 10, Month: 12, Year: 2019}, {Day: 10, Month: 1, Year: 2020}) // true
+
+var checkerIfMonthApart = function(date1, date2) {
+  if (date1['Day'] !== date2['Day']) {
+    return false
+  }
+
+  let isOneMonthApart = date1['Month'] + 1 == date2['Month'] || date1['Month'] - 1 == date2['Month']
+  
+  // Ideally should be a function by itself. Repeating things basically:
+  let isItANewYearAnd1MonthApart = 
+    (date1['Month'] == 12 && date2['Month'] == 1 && date1['Year'] + 1 == date2['Year']) 
+  || (date1['Month'] == 1 && date2['Month'] == 12 && date1['Year'] - 1 == date2['Year']) 
+                                      
+  if(isItANewYearAnd1MonthApart == true) {
+    return true;
+  }                                     
+  
+  if(date1['Year'] == date2['Year'] && isOneMonthApart)  {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
+// If there's a new year
+console.log(checkerIfMonthApart({Day: 10, Month: 1, Year: 2020}, {Day: 10, Month: 12, Year: 2019})) // true
+
+// Date needs to be the same
+console.log(checkerIfMonthApart({Day: 11, Month: 9, Year: 2020}, {Day: 10, Month: 10, Year: 2020})) // false
+
+// Month needs to be ONLY off by 1
+console.log(checkerIfMonthApart({Day: 10, Month: 9, Year: 2020}, {Day: 10, Month: 10, Year: 2020})) // true
+console.log(checkerIfMonthApart({Day: 10, Month: 9, Year: 2020}, {Day: 10, Month: 8, Year: 2020})) // true
+console.log(checkerIfMonthApart({Day: 10, Month: 9, Year: 2020}, {Day: 10, Month: 2, Year: 2020})) // false
+console.log(checkerIfMonthApart({Day: 10, Month: 9, Year: 2020}, {Day: 10, Month: 12, Year: 2020})) // false
+
+// Year needs to be the same
+console.log(checkerIfMonthApart({Day: 10, Month: 9, Year: 2020}, {Day: 10, Month: 10, Year: 2020})) // true
+console.log(checkerIfMonthApart({Day: 10, Month: 9, Year: 2020}, {Day: 10, Month: 10, Year: 2019})) // false
+
+
+
+
+
+
