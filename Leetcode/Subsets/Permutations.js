06dd54e9fe82f9d359efcc8db73c1433a242babf
indykiss <indirak@alumni.princeton.edu>
2020-10-28 16:17:11 -0400

    add permutations

---
+++ Leetcode/Subsets/Permutations.js
@@ -0,0 +1,57 @@
+
+
+/*
+Permutations
+
+Given a collection of distinct integers, return all possible permutations.
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+If a set has n distinct eles, it'll have n! (exponential) permutations. 
+
+Strat: 
+- Vars: result arr, currArr, visited hash 
+    Visited: {num: have we seen this num}
+- 
+*/
+
+
+// Do me again 
+// Oct, FB/ Bloomb. 
+var permute = function(nums) {
+    let res = [];
+    
+    backtrack();
+    
+    return res;
+    
+    // Recursive: 
+    function backtrack(currArr = [], visited = {}) {
+        // base case: 
+        if(currArr.length === nums.length) {
+            res.push(currArr.slice()); // we copy arr and add to res
+            // IF our curr arr is a permutation of nums 
+        }
+        
+        // recursive action. we look at every num and add it into the currArr 
+        for(let num of nums) {
+            if(!visited[num]) {
+                visited[num] = true // we've seen this num so lets add
+                currArr.push(num); 
+                backtrack(currArr, visited) 
+                currArr.pop(); // now we backtrack? 
+                visited[num] = false // now we processed it, so now false
+            }
+        }
+    }
+};
+
