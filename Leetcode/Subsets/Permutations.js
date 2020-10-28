e1304bce5da2067672c8a88a2ff9e887a9edfb69
indykiss <indirak@alumni.princeton.edu>
2020-10-28 16:44:58 -0400

    add permutations

--- Leetcode/Subsets/Permutations.js
+++ Leetcode/Subsets/Permutations.js
@@ -54,4 +54,30 @@ var permute = function(nums) {
         }
     }
 };
+// Time: O(n!) time. Because we need to increase output exponentially
+// for every input increase 
 
+
+// Grokking approach:
+function generatePermutations(nums) {
+    let res = [];
+
+    generatePermutations(nums, 0, [], res);
+
+    return res; 
+
+
+    function generatePermutations(nums, idx, currArr, res) {
+        // base case. we're set to add this currArr to res 
+        if(idx === nums.length) {
+            res.push(currArr);
+        } else {
+        // now let's add each num into currArr if we must 
+        for(let i = 0; i < currArr.length + 1; i++) {
+            let newPermute = currArr.slice(0) // clone 
+            newPermute.splice(i, 0, nums[idx]); // insert nums[i] at index i
+            permute(nums, idx + 1, currArr, res); // recurse action
+        }
+        }
+    }
+}
