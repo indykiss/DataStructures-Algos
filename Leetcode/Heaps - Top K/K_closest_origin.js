5af57f834234afb877e6ce264823c3b38782a93e
indykiss <indirak@alumni.princeton.edu>
2020-10-31 14:28:33 -0400

    add k closest origin with maxHeap

--- Leetcode/Heaps - Top K/K_closest_origin.js
+++ Leetcode/Heaps - Top K/K_closest_origin.js
@@ -2,11 +2,13 @@
 
 /*
 K Closest Points to Origin
-We have a list of points on the plane. Find the K closest points to the origin (0, 0).
+We have a list of points on the plane. Find the K closest points 
+to the origin (0, 0).
 
 (Here, the distance between two points on a plane is the Euclidean distance.)
 
-You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in.)
+You may return the answer in any order. The answer is guaranteed to 
+be unique (except for the order that it is in.)
 
 Strategy:
 - For each point on the grid, we want to calculate distance 
@@ -14,6 +16,49 @@ Strategy:
 - Then we want to slice the array until k 
 */
 
+
+
+// Oct, Grokking way to do this with a heap. 
+// Fb/ Bloomb. Max heap! Yay, not terrible if I'm importing :) 
+const Heap = require('./collections/heap'); // import Heap. syntax? 
+
+var kClosest = function(points, K) {
+    
+    let maxHeap = new Heap([], null, (a,b) => a-b);
+    for(let i = 0; i < K; i++) {
+        maxHeap.push(points[i]);
+    }
+    
+    for(let idx = 0; idx < points.length; idx++) {
+        let curr = points[idx];
+        let currDist = distanceCalculator(curr);
+        let rootOfMaxHeap = maxHeap.peek();
+        let rootDist = distanceCalculator(rootOfMaxHeap); 
+        
+        if(currDist < rootDist) {
+            maxHeap.pop();
+            maxHeap.push(curr);
+        }        
+    }
+    
+    return maxHeap.toArray();
+};
+
+function distanceCalculator(coords) {
+    let x = coords[0],
+        y = coords[1];
+    
+    return x^2 + y^2; 
+}
+// O(N * log k) time and O(K) space
+
+
+
+
+
+// Warm up, easy way to do this. 
+// IF ASKED THIS Q, DO IT THIS WAY FIRST AND SAY WE CAN DO THIS 
+// WITH A HEAP INSTEAD. ALWAYS DO BRUTE FORCE FIRST 
 var kClosest = function(points, k) {
     points = points.sort((a,b) => distance(a) - distance(b));
     return poits.slice(0,k);
