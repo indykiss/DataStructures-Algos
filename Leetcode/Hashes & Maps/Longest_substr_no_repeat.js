c1aabafcc2562eaf48a4093e92c2262586cdfa53
indykiss <indirak@alumni.princeton.edu>
2020-11-05 17:34:03 -0500

    refresh longest substr without repeating chars

--- Leetcode/Hashes & Maps/Longest_substr_no_repeat.js
+++ Leetcode/Hashes & Maps/Longest_substr_no_repeat.js
@@ -21,6 +21,43 @@ Note that the answer must be a substring, "pwke" is a subsequence and not a subs
 */
 
 
+/*
+Strat: Sliding window
+- Vars: Set that tracks the uniq chars in the window 
+    - Length that is updated as we move the window alone
+    - We'll have a start and end of the window. 
+- Iterate thru str. Before we add a char in, check if the new char is uniq by checking set.    
+    - If curr char is not uniq, we want to remove the first 
+ele in the set, using start++ and delete, until we don't have the new curr char in there. 
+- Add to set. In each iteration, update length. 
+
+Nov. Mostly got this one. Needed 1 hint 
+*/
+var lengthOfLongestSubstring = function(s) {    
+    let start = 0,
+        set = new Set(),
+        length = 0;
+    
+    for(let end = 0; end < s.length; end++) {
+        
+        // If set has char, delete eles from set until 
+        // that char is no longer in it
+        if(set.has(s[end])) {
+            while(set.has(s[end])) {
+                set.delete(s[start]);
+                start++;
+            }
+        }
+        // Add this ele to the set
+        set.add(s[end]);            
+        // Update length for max    
+        length = Math.max(length, end - start + 1)
+    }
+    return length;
+}
+
+
+
 // Sliding window approach. Oct, FB
 var lengthOfLongestSubstring = function(s) {    
 
