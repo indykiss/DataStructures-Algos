36e0fd57c8c8781eeb6fd5f3b9e0d7c4ec1e09b7
indykiss <indirak@alumni.princeton.edu>
2020-11-05 17:53:59 -0500

    refresh is palindrome number

--- Leetcode/Warm up/Palindrome_number.js
+++ Leetcode/Warm up/Palindrome_number.js
@@ -14,6 +14,39 @@ Also need to check if we've hit the middle num, it's still a
 palindrome if length of num is odd. 
 */
 
+
+var isPalindrome = function(x) {
+    if(x < 0) return false;
+    
+    let str = x.toString(),
+        start = 0,
+        end = str.length -1;
+    
+    while(start < end) {
+        if(str[start] !== str[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+
+
+/*
+Strategy:
+- If negative, not possible to be a palindrome
+- Num => str => arr => reverse => join to make str
+- Return reversed === x
+
+*/
+var isPalindromeBuiltIn = function(x) {
+    if(x < 0) return false;
+    let rev = x.toString().split('').reverse().join('');
+    return rev == x;
+}
+
+
 var isPalindrome = function(x) {
     let str = x.toString();
     
