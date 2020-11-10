7050f426fc9f74a66ca21ab5a3dce0957a27b6b2
indykiss <indirak@alumni.princeton.edu>
2020-11-10 11:24:20 -0500

    refresh find kth largest element in an arr

--- Leetcode/Heaps - Top K/Kth_largest_ele.js
+++ Leetcode/Heaps - Top K/Kth_largest_ele.js
@@ -1,7 +1,8 @@
 /*
 Kth Largest Element in an Array
 
-Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Find the kth largest element in an unsorted array. Note that it is the 
+kth largest element in the sorted order, not the kth distinct element.
 
 Input: [3,2,1,5,6,4] and k = 2
 Output: 5
@@ -9,9 +10,73 @@ Output: 5
 Strategy:
 - Sort the arr
 - We want the KTH largest
-- Loop starting from the end. Once we hit arr.length - 1 - K, return that value
+- Loop starting from the end. Once we hit arr.length - 1 - K, 
+return that value
 
 */
+
+
+
+// Nov
+// 30 mins to maxHeap + loop way 
+
+/*
+Strat: MaxHeap. 
+- Root of a maxheap = largest number SO we can 
+loop from o - k and pull off the root until we hit k. 
+When we hit k, return that value
+
+Space: O(N) where n is num of eles within arr, bc we have a maxheap
+Time: O(n) to build heap. But O(n log n) to remove root from maxheap
+
+Strat: 
+- Sort the arr asc then ID k -1 index and return it 
+- Add eles to a set to get uniq values
+Time: O(n log n), sort
+Space: O(n) bc set can hold all eles 
+
+Strat:
+- Sort the arr
+- Loop thru arr, track a counter that counts uniq vals 
+- once counter === k, return nums[i]
+*/
+
+// [1, 3, 4, 3, 1], k = 3, 4
+var findKthLargest = function(nums, k) {
+    if(nums.length < 2) return nums;
+    
+    nums = nums.sort((a,b) => b-a);
+        
+    return nums[k - 1];
+};
+
+
+
+const Heap = require("./collection/heap") // import heap 
+
+var findKthLargestHeap = function(nums, k) {
+    
+    let maxHeap = new Heap([], null, (a,b) => b-a); 
+        
+    // add nums to maxheap
+    for(let num of nums) {
+        maxHeap.push(num);
+    }
+    
+    // ID the kth largest ele
+    for(let i = 0; i < k; i++) {
+        let ele = maxHeap.pop(); // removes biggest num in heap, root 
+        
+        if(i === k) {
+            return ele;
+        }
+    } 
+}
+
+
+
+
+
 
 
 // Min heap:
