3e946d3b7352f4f551abb006f3e20de614d2e57e
indykiss <indirak@alumni.princeton.edu>
2020-11-14 17:14:29 -0500

    refresh LRUcache

--- Leetcode/Fall Practice/Interviewingio.js
+++ Leetcode/Fall Practice/Interviewingio.js
@@ -330,6 +330,50 @@ for( let [key, val] of Map)
  */
 
 
+
+/*
+Constructor: this.capacity, this.cache is a map
+
+Get:
+- Return val of key if exists. Else -1
+Since we're getting, we'd want to readd to refresh usage 
+
+Put:
+- Update the value of the key if it exists, or add if it doesnt
+- If capacity is reached, remove the least recently used ele
+*/
+
+class LRUCache {
+  constructor(capacity) {
+      this.capacity = capacity; 
+      this.cache = new Map();
+  }
+  
+  get(key) {
+      if(this.cache.has(key)) {
+          let temp = this.cache.get(key);
+          this.cache.delete(key);
+          this.cache.set(key, temp);
+          return temp; 
+      } else {
+          return -1;
+      }
+  }
+  
+  put(key, value) {
+      if(this.cache.has(key)) {
+          this.cache.delete(key);
+      }  
+      this.cache.set(key, value);
+      
+      if(this.cache.size > this.capacity) {
+          let leastUsed = this.cache.keys().next().value;
+          this.cache.delete(leastUsed);
+      }
+  }
+}
+
+
 class LRUCache {
   constructor(capacity) {
     console.log("Hello Again")
