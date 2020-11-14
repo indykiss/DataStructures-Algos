49bbdb8487cb1016258e3a65c2f0c3f31e6985f2
indykiss <indirak@alumni.princeton.edu>
2020-11-13 21:16:35 -0500

    refresh minimum remove to make valid parentheses

--- Leetcode/Stacks and Queues/Minimum_remove_parentheses.js
+++ Leetcode/Stacks and Queues/Minimum_remove_parentheses.js
@@ -14,6 +14,46 @@ Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
 
 */
 
+
+/* 
+Strat: 
+- Convert to arr so easy to handle
+- Stack that holds openers' indices
+- If we find a closer, we can remove an opener from stack 
+so that's a matching set. Nothing to remove here 
+- If we find a closer when there is nothing left in stack, 
+we can set this new closer equal to "" 
+- At the end of 1st loop, we loop through stack of indices, 
+and set arr[index] = ""
+- Return arr.join('');
+*/
+
+// Was fine. Took 30 but got close. 
+var minRemoveToMakeValid = function(s) {
+    let arr = s.split(''),
+        stack = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+        let char = arr[i];
+        if(char == '(') {
+            stack.push(i)
+        }
+        else if (char == ')') {
+            if(stack.length > 0) stack.pop();
+            else arr[i] = '';
+        }
+    }
+    
+    // we also need to handle extra openers 
+    // at each index saved in stack
+    for(let index of stack){
+        arr[index] = "";
+    }
+    return arr.join('');
+};
+
+
+
 var minRemoveToMakeValid = function(s) {
     
     let arr = s.split(''),
