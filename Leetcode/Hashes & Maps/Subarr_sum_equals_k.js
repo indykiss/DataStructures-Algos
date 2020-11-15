26cc81828be3ab28c8f22f82fb3fa58023b9edd1
indykiss <indirak@alumni.princeton.edu>
2020-11-15 13:31:40 -0500

    refresh subarr sum equals k

--- Leetcode/Hashes & Maps/Subarr_sum_equals_k.js
+++ Leetcode/Hashes & Maps/Subarr_sum_equals_k.js
@@ -19,9 +19,73 @@ A hash does not equal a map.
 */
 
 
+/*
+Strat: Brute force
+- Counter
+- Nested loops 
+    - Outer: Looks at 1st num in subarr
+    - Inner: Looks at the nums after 1st num 
+    - Calculate sum. If sum == k, count++ 
+        - If sum > k, break inner loop 
+- Return counter
+*/
+
+
+// Nov, 20 mins
+// Intuitive. OK to do this on interview.
+// Doesn't pass leetcodes but thats fine
+// Time: O(n), O(1) space b/c counter 
+var subarraySumIntuit = function(nums, k) {
+    let counter = 0; 
+    
+    for(let i = 0; i < nums.length; i++) {
+        let sum = nums[i];
+        
+        for(let j = i+1; j < nums.length; j++) {
+            sum += nums[j];
+            
+            if(sum === k) {
+                counter++;
+            }
+            if(sum > k) {
+                break;
+            }            
+        }   
+    }
+    return counter; 
+} 
+// Edge: zeros, [1, 1, 0, 1],  2  => 3
+
+
+// Optimal solution, not intuitive. 
+var subarraySum = function(nums, k) {
+    const map = new Map();
+    let count = 0;
+    let sum = 0;
+    
+    map.set(0,1);
+    for(let i = 0; i < nums.length; i++) {
+        let cur = nums[i];
+        sum = sum + cur;
+        let partner = sum - k
+        
+        if(map.has(partner)) {
+            count += map.get(partner);
+        } 
+        if(map.has(sum)) {
+            map.set(sum, map.get(sum) + 1);
+        } else {
+            map.set(sum, 1)            
+        }        
+    }
+
+    return count;
+}
+
+
+
 
 // Attempt #3. Blegh. I'm not good at this
-
 var subarraySum = function(nums, k) {
     const map = new Map();
     let count = 0;
