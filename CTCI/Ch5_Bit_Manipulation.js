4673e80e6fdd0358a10b094cacbde3f947ece6de
indykiss <indirak@alumni.princeton.edu>
2021-01-31 12:58:29 -1000

    add checklis + bit chapter

---
+++ CTCI/Ch5_Bit_Manipulation.js
@@ -0,0 +1,23 @@
+
+
+/*
+Bit manipulation  / Bit Math
+
+Bits are base 2 nums, where 0 = no and 1 = yes; 
+16-8-4-2-1 etc. 
+1 1 1 1 1 = 16 + 8 + 4 + 2 + 1 = 31 
+
+OR, using two's complement ie negative num, 
+where the first num is neg: 
+- 16 + 8 + 4 + 2 + 1 = -1 
+
+
+Ex:
+0110 + 0010 = 1000
+*/
+
+// Top 3 Bit questions:
+
+
+// Binary to String: 
+
