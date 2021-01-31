df9082742272296d42f9b2f162616851b499bbcc
indykiss <indirak@alumni.princeton.edu>
2021-01-31 13:08:54 -1000

    pause CTCI, bit manipulation

--- CTCI/Ch5_Bit_Manipulation.js
+++ CTCI/Ch5_Bit_Manipulation.js
@@ -19,5 +19,68 @@ Ex:
 // Top 3 Bit questions:
 
 
-// Binary to String: 
+// Binary to String: Convert a string into binary 
 
+
+
+
+// Add Binary: Given two binary strs a and b, return their sum as a binary string.
+
+
+function addBinary(a, b) {
+
+    let convertA = BigInt("0b" + a), 
+        convertB = BigInt("0b" + b); 
+    
+        return (convertA + convertB).toString(2);
+}
+
+/*
+Strat:
+- Make 2 pointers
+- Process each individual addition by digit place 
+as we run through the length of both nums 
+*/
+
+function addBinaryNoLib(a,b) {
+    let sum = "";
+
+    let r1 = a.length - 1, 
+        r2 = b.length - 1,
+        carry = false; 
+
+    while(r1.length >= 0 || r2.length >= 0) {
+        sum = sum + binaryAdder(r1, r2); 
+        r1--;
+        r2--;
+    }
+
+    function binaryAdder(r1, r2) {
+        let miniSum = 0; 
+
+        if(carry) {
+            miniSum++; 
+            carry = false; 
+        }
+
+        if(a[r1] === "1") {
+            miniSum++;
+        }
+        if(b[r2] === "1") {
+            miniSum++;
+        }
+
+        if(miniSum >= 2) {
+            carry = true; 
+            miniSum = miniSum - 2; 
+        }  
+
+        return miniSum; 
+    }
+
+    if(carry) {
+        sum = "1" + sum; 
+    }
+
+    return sum; 
+}
