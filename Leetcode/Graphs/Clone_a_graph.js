c17aaaa33feda1bd5d0fbf72cffcb39929525cb0
indykiss <indirak@alumni.princeton.edu>
2021-02-06 21:28:24 -0500

    refresh clone graph

--- Leetcode/Graphs/Clone_a_graph.js
+++ Leetcode/Graphs/Clone_a_graph.js
@@ -38,6 +38,34 @@ Explanation: There are 4 nodes in the graph.
 */
 
 
+
+// Using a hash table:
+var cloneGraph = function(node) {
+    if (!node) return node
+
+    let queue = [node],
+        hash = {}
+
+    hash[node.val] = new Node(node.val)
+
+    while (queue.length > 0) {
+        const curr = queue.shift();
+        curr.neighbors.forEach(n => {
+            // if n is not yet visited
+            if (hash[n.val] === undefined) {
+                // copy n to the new graph
+                hash[n.val] = new Node(n.val);
+                // enqueue n
+                queue.push(n);
+            }
+            // connect the current new node with its neighbors in the new graph
+            hash[curr.val].neighbors.push(hash[n.val]);
+        })
+    }
+    return hash[node.val];
+};
+
+// Using a map:
 var cloneGraph = function(node) {
     if(node === null) return null;
     
