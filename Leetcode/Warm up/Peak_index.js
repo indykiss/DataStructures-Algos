483336871260148a038bb90a330d6359ed2d807c
indykiss <indirak@alumni.princeton.edu>
2021-02-14 23:12:08 -0500

    add find peak

--- Leetcode/Warm up/Peak_index.js
+++ Leetcode/Warm up/Peak_index.js
@@ -30,3 +30,53 @@ var peakIndexInMountainArray = function(arr) {
     }
     return peak;
 };
+
+
+/*
+Slightly different problem: 
+  Similar to find peaks problem, you are given an array
+  of numbers. The goal here is to return "any one" peak that exist in
+  the array. The array will always consist of at least one peak.
+  Your solution must run in O(log n) time at worst case.
+ 
+  Input: [3,4,6,9,7,6,2,5,1]
+  Output: 9 or 5 are both valid outputs. They are both peaks.
+  But you only return one of them.
+ 
+  Strategy: Binary search
+   [3, 4, 6, 9, 7]
+  - Start, end: Use these to find peak between st/mid/end
+     - While loop, mk midpt
+     - If A[mid] < A[mid + 1]
+         - low = mid + 1
+     - else
+         - end = mid
+   - Return start
+ 
+  Edge cases:
+  - Zero peaks : Still need O(log n) time
+  - [1, 2, 3, 4] => [4]
+  - [1, 1, 1, 1] => [1]
+  - Thought: need more space to make more time efficient??
+*/
+
+
+// O(log n) time:
+function findPeak(arr) {
+    let start = 0,
+      end = arr.length - 1;
+  
+    while (start < end) {
+      const mid = Math.floor((start + end) / 2);
+  
+      // found a peak
+      if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+        return mid;
+        // look left
+      } else if (arr[mid] > arr[mid - 1]) {
+        start = mid;
+        // look right
+      } else {
+        end = mid;
+      }
+    }
