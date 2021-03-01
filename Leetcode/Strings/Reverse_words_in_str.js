33a19c8665ad9bc6fb8c9233e3c07bc6156169ce
indykiss <indirak@alumni.princeton.edu>
2021-03-01 16:40:39 -0500

    practice reverse words in str

--- Leetcode/Strings/Reverse_words_in_str.js
+++ Leetcode/Strings/Reverse_words_in_str.js
@@ -71,6 +71,66 @@ var reverseWords = function(s) {
 
 
 
+// No split or reverse: Make own reverse 
 
+/*
 
+Strategy:
+- Use a stack to hold the words as we iterate thru str 
+- Add to stack word. Each word has a start & end pt,where end at whitespace. 
+	Cant start at a whitespace 
+- Reverse stack with helper 
+- Return stack.join(" ")
+
+"fb is great" 
+start = 0, end = 0 
+iterate
+once we hit whitespace: 
+	end = i; 
+	trim extra whitespace: 
+	while(str[start] == " ") ? 
+		start++
+	add to stack substr of str.slice(start,end); 
+	start = end + 1; 
+keep iterating until next whitespace 
+
+stack = [fb, is, great] 
+
+iterate thru stack - pop ele, add to resultString + " "
+
+Edge:
+- "facebook   is great" => "great is facebook"
+
+*/
+
+function reverseWordsInString(string) {
+    let stack = [], 
+              start = 0;
+      
+      for(let i = 0; i < string.length; i++) {
+          if(string[i] === " ") {
+              stack.push(string.slice(start, i));  
+              start = i; 
+          } else if (string[start] === " ") {
+              stack.push(" "); 
+              start = i; 
+          }
+      }
+      
+      stack.push(string.slice(start)); // we lost 1st word so add it in? 
+      
+      reverseStack(stack);
+      return stack.join("");
+  }
+  
+  function reverseStack(stack) {
+      let start = 0, 
+              end = stack.length - 1; 
+      
+      while(start < end) {
+          [stack[start], stack[end]] = [stack[end], stack[start]];
+          start++; 
+          end--; 
+      }
+  }
 
