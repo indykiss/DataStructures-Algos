582b5502a7cd332f8d9ba789d267f4a3d5a2c929
indykiss <indirak@alumni.princeton.edu>
2021-03-09 19:19:59 -0500

    practice path sum II

--- Leetcode/Depth first search/Path_sum_II.js
+++ Leetcode/Depth first search/Path_sum_II.js
@@ -32,6 +32,55 @@ Strat:
 
 */
 
+
+/*
+Strat: O(n) time and O(n) space, num of nodes 
+- DFS to ID all possible paths 
+- Base case, recursive action, trigger to add
+a path to the res arr
+- have arr to hold path.
+
+- DFS as a helper function 
+    - base: 
+        - if we've hit the last left && currNode.val === target sum: add path arr to res arr 
+    - pathArr.push(root.val); 
+    - recursive: 
+        - return dfs(root.left, targetSum - root.val) && dfs(root.right, targetSum - root.val, pathArr)
+        
+Edge:
+- No paths = targetSum
+- All paths = targetSum 
+- Invalid input, if(!root) return []; 
+- Unbalanced tree possible 
+- Can any root.val be negative? No 
+*/
+
+
+// dfs recursive practice, march
+function pathSum(root, targetSum) {
+    if(!root) return [];
+    let res = []; 
+    
+    dfs(root, [], 0); 
+    
+    return res;
+    
+    function dfs(root, currPath, pathSum) {
+        if(root === null) return; 
+        
+        pathSum += root.val; 
+        currPath.push(root.val); 
+        
+        if(!root.left && !root.right && pathSum === targetSum) {
+            res.push(currPath); 
+        }
+        
+        if(root.left) dfs(root.left, [...currPath], pathSum);
+        if(root.right) dfs(root.right, [...currPath], pathSum);
+    }
+}
+
+
 // recursive. Oct, fb/ bloomb 
 var pathSum = function(root, target) {
     let paths = [];
