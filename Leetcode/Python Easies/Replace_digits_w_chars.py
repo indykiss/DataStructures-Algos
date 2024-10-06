8b9ed6c5b3c8fe06546c0e068cb13a125c0b3d74
Indira Kissoondyal <indirak@alumni.princeton.edu>
2024-10-06 16:04:27 -0400

    Update Replace_digits_w_chars.py

--- Leetcode/Python Easies/Replace_digits_w_chars.py
+++ Leetcode/Python Easies/Replace_digits_w_chars.py
@@ -7,6 +7,24 @@
 # - s[3] -> shift('c',1) = 'd'
 # - s[5] -> shift('e',1) = 'f'
 
+# a1c1e1 
+# abcdef, bitwise
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        # take in char and x to add # to shift, then convert back to char 
+        def shift(og_char, num_to_shift): 
+            return chr(ord(og_char) + num_to_shift)
+        
+        arr = list(s)
+        res = [""] * len(arr)
+        for i, ch in enumerate(arr):
+            prev = arr[i-1] 
+            if ch.isdigit():
+                res[i] = shift(prev, int(ch))
+            else:
+                res[i] = ch
+        return "".join(res)
+
 
 class Solution:
     def replaceDigits(self, s: str) -> str:
